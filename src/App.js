3c4bc6dc427ded2aeec994e7017cca5b09e3de03
Martin Dallos <tinypin14@gmail.com>
2023-04-30 13:33:44 +0200

    Added footer comp. Finished static components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './style//App.css';
 import Header from './components/Header';
 import Gameboard from './components/Gameboard';
+import Footer from './components/Footer';
 
 function App() {
   return (
       <div className='wrapper'>
         <Header />
         <Gameboard />
+        <Footer />
       </div>
     );
 }
