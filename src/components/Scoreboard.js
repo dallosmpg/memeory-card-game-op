e33a148639ee5e307749a9960b965b906707d2ef
Martin Dallos <tinypin14@gmail.com>
2023-05-05 21:37:46 +0200

    Aesthetic and UI changes

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -3,12 +3,14 @@ import React, {useState, useEffect} from "react";
 export default function Scoreboard({clickedImages, setClickedImages}) {
     const [currentScore, setCurrentScore] = useState(0);
     const [bestScore, setBestScore] = useState(0);
+    const [isError, setIsError] = useState(false);
 
     useEffect(() => {
         if (clickedImages.length < 1) return;
         if (new Set(clickedImages).size !== clickedImages.length) {
             setCurrentScore(0);
             setClickedImages([]);
+            setIsError(true);
         } else {
             setCurrentScore(prevScore => prevScore + 1);
         }
@@ -17,11 +19,17 @@ export default function Scoreboard({clickedImages, setClickedImages}) {
 
     useEffect(() => {
         if (currentScore > bestScore) setBestScore(currentScore);
-    }, [currentScore])
+    }, [currentScore]);
+
+    useEffect(() => {
+        if (isError) setTimeout(() => {
+            setIsError(false);
+        }, 2000)
+    }, [isError])
     
     return (
         <div className="header scoreboard flex-center-column">
-            <h3>Current score: {currentScore}</h3>
+            <h3 className={isError ? 'error' : ''}>Current score: {currentScore}</h3>
             <h3>Best score: {bestScore}</h3>
         </div>
     )
